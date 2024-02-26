c1aba92340d3c4c2dd95997bc57d998cd7af54ec
Tanveer Sayem <tanveersayem@gmail.com>
2024-02-26 11:58:39 -0500

    initial commit

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -5,7 +5,7 @@ export default function InvoiceStatus({ status }: { status: string }) {
   return (
     <span
       className={clsx(
-        'inline-flex items-center rounded-full px-2 py-1 text-xs',
+        'inline-flex items-center rounded-full px-1 py-1 text-sm',
         {
           'bg-gray-100 text-gray-500': status === 'pending',
           'bg-green-500 text-white': status === 'paid',
