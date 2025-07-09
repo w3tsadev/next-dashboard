b064b71eb5905f2318095df7b2c81c75b767507a
Coding Jitsu <115426204+w3tsadev@users.noreply.github.com>
2025-07-08 20:05:43 -0400

    Update auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -7,7 +7,7 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const isOnDashboard = nextUrl.pathname.startsWith('...'); // /dashboard
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
